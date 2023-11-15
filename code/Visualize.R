f1fb24c82078ce789b85d27bb16c26553a4c1943
Grace Henry <henryn98@uw.edu>
2023-11-15 15:35:44 -0800

    Update Visualize.R

--- code/Visualize.R
+++ code/Visualize.R
@@ -7,6 +7,11 @@ library(ggplot2)
 #read in the main data file
 data <- read.csv(file="main.data")
 
+#average points that have replicates
+
+
+
+
 #scatter plot of year vs isotope signature
 ggplot(data = data, aes(Year, PHE.mean, color = System)) +
        geom_point(size = 3, alpha = 0.7) 
@@ -16,7 +21,17 @@ ggplot(data = data, aes(Year, PHE.mean, color = System)) +
 ggplot(data = data, aes(Year, PHE.mean, color = System)) +
   geom_line(size = 1, alpha = 0.7) 
 
-Wood <- 
+Wood <- data[data$System =="Wood",]
+Kvichak <- data[data$System =="Kvichak",]
+Egegik <- data[data$System =="Egegik",]
 
-?subset()
+#plots of age vs isotope sig in the different river systems 
+ggplot(data = Wood, aes(Year, PHE.mean, color = as.character(Age))) +
+  geom_point(size = 3, alpha = 0.7) +
+  geom_smooth(method = "lm")
 
+ggplot(data = Kvichak, aes(Year, PHE.mean, color = as.character(Age))) +
+  geom_point(size = 3, alpha = 0.7) 
+
+ggplot(data = Egegik, aes(Year, PHE.mean, color = as.character(Age))) +
+  geom_point(size = 3, alpha = 0.7) 
