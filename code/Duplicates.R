e048ed5d29489eb437dc4bec5f4bf22852ffcd67
Grace Henry <henryn98@uw.edu>
2023-11-21 12:55:41 -0800

    edited

--- code/Duplicates.R
+++ code/Duplicates.R
@@ -52,5 +52,29 @@ rm_duplicates <- function(df, ID, Year, System, Age){
 data <- rm_duplicates(df = data, ID = "22_K_3", Year = "2022", System = "Kvichak", Age = "3")
 data <- rm_duplicates(df = data, ID = "22_W_3", Year = "2022", System = "Wood", Age = "3")
 
-duplicated(data$Sample.ID)
+#check to see if there are any duplicate samples left 
+anyDuplicated(data$Sample.ID)
+
+#get rid of duplicates before dealing with replicate samples
+#start by removing sample ID _R which identifies replicates
+rep <- substr(data$Sample.ID, 1, 6)
+data$rep <- substr(data$Sample.ID, 1, 6)
+which(duplicated(rep))
+
+rep[42]
+E042 <- subset(data, rep == "04_E_2")
+
+rep[43]
+W102<- subset(data, rep == "10_W_2")
+
+rep[58]
+E013 <- subset(data, rep == "01_E_3")
+
+rep[60]
+K892 <- subset(data, rep == "89_K_2")
+
+rep[63]
+K223 <- subset(data, rep == "22_K_3")
+
+df <- rbind(E042, W102, E013, K892, K223)
 
