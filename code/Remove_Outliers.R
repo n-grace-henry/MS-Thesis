6813acb9ac8ad080e361d02125751824619102ee
Grace Henry <henryn98@uw.edu>
2023-11-28 09:32:29 -0800

    edited

---
+++ code/Remove_Outliers.R
@@ -0,0 +1,11 @@
+#script to compile all post processed csv files into one csv file
+#use this after the DriftCorrection.R script
+setwd("~/Documents/GitHub/CSIA_lab_work/data/DriftCorrected_noMean")
+
+library(dplyr)
+library(readr)
+
+#compile all the csv files to make one dataframe of all data
+all.df <- list.files(path="~/Documents/GitHub/CSIA_lab_work/data/DriftCorrected_noMean") %>% 
+  lapply(read_csv) %>% 
+  bind_rows 
