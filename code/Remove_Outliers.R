50d13d10ca4ff673c6aeb1b237fcfb9369288093
Grace Henry <henryn98@uw.edu>
2023-11-29 10:55:20 -0800

    edited

--- code/Remove_Outliers.R
+++ code/Remove_Outliers.R
@@ -24,13 +24,8 @@ PHE <- subset(all.data, AAID == "PHE")
 #find the differences between the three injections (triplicates)
 #if any value is 3 or more times higher than the other differences then it counts as an outlier
 #for samples that have duplicates, I will only be looking at the triplicates from the same run
-#for example, if 10_W_2 was run on two different days, I will treat each set of three as there own and not compare differences over the 6 injections
-
-min(abs(PHE$adj[1]-PHE$adj[2]),
-    abs(PHE$adj[2]-PHE$adj[3]),
-    abs(PHE$adj[1]-PHE$adj[3])) * 3 <= max(abs(PHE$adj[1]-PHE$adj[2]),
-                                           abs(PHE$adj[2]-PHE$adj[3]),
-                                           abs(PHE$adj[1]-PHE$adj[3]))
+#for example, if 10_W_2 was run on two different days
+#I will treat each set of three as there own and not compare differences over the 6 injections
 
 for(i in 1:(length(PHE$adj)/3)){
   min(abs(PHE$adj[i]-PHE$adj[i+1]),
@@ -41,12 +36,23 @@ for(i in 1:(length(PHE$adj)/3)){
   
 }
 
-unique(PHE$ID1)
-a <-PHE[PHE$ID1 == "22_W_2","adj"]
+#changing the format of the data to make visualization of each sample easier
+samples <- vector(length = 79)
+for(i in 0:78){
+  samples[i+1] <- PHE$ID1[(i*3)+1]
+}
 
-i=1
-i=4
-i=7
-i=10
+wide.format <- data.frame(matrix(nrow=4, ncol=79))
+for(i in 1:79){
+  wide.format[1,i] <- samples[i]
+}
+
+for(i in 0:78){
+  wide.format[2,i+1] <- PHE$adj[(i*3)+1]
+  wide.format[3,i+1] <- PHE$adj[(i*3)+2]
+  wide.format[4,i+1] <- PHE$adj[(i*3)+3]
+}
+
+
 
 
