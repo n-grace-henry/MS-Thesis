3a698847d898f09a76f8b2b0917930badb4c4499
Grace Henry <henryn98@uw.edu>
2024-02-07 11:33:34 -0800

    edited

---
+++ code/Correct_to_VPDB.R
@@ -0,0 +1,14 @@
+setwd("~/Documents/GitHub/CSIA_lab_work/data/SIA results")
+
+#Code to take raw data referenced to nitrogen tank and convert to the 
+#VPDB international standard
+#Looking at three separate runs and will be comparing corrections found 
+#across runs
+
+
+
+### First EA run ###
+#Load in data
+data <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/SIA results/230914_Jameson_10B-AqPlants.csv")
+
+
