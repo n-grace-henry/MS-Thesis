724701cea745a6684e1b49ad4022a77dfad5eccd
Grace Henry <henryn98@uw.edu>
2024-02-09 11:31:23 -0800

    edited

--- code/Correct_to_VPDB.R
+++ code/Correct_to_VPDB.R
@@ -8,8 +8,7 @@ setwd("~/Documents/GitHub/CSIA_lab_work/data/SIA results")
 #notation as final step 
 
 
-
-### First EA run ###
+####### Data Set One #######
 #Load in data
 data <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/SIA results/230914_Jameson_10B-AqPlants.csv")
 
@@ -51,8 +50,73 @@ PCH <- data[data$Identifier.1 %in% c("PCH_STD_01",
 PCH.avg <- mean(PCH)
 
 #Find offset values
-GA1.off <- GA1.avg.R - GA1.r 
-GA2.off <- GA2.avg.R - GA2.r
-PCH.off <- PCH.avg.R - PCH.r
+GA1.off <- GA1.avg- GA1.ref
+GA2.off <- GA2.avg - GA2.ref
+PCH.off <- PCH.avg - PCH.ref
+
+#Plot 
+offset <- c(GA1.off, GA2.off, PCH.off)
+measured <- c(GA1.avg, GA2.avg, PCH.avg)
+
+plot(x = measured, y = offset)
+
+#Linear regression
+lm.1 <- lm(offset ~ measured)
+summary(lm.1)
 
 
+####### Data Set Two #######
+#Load in data
+data <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/SIA results/230914_Jameson_10B-AqPlants.csv")
+
+#True values referenced to air
+GA1.ref <- -4.6
+GA2.ref <- -5.7
+PCH.ref <- 1.98
+SAL.ref <- 11.3
+
+#Grab all GA1 data
+GA1 <- data[data$Identifier.1 %in% c("GA1_QTY_2", 
+                                     "GA1_STD_01", 
+                                     "GA1_STD_02", 
+                                     "GA1_STD_03", 
+                                     "GA1_STD_04") &
+              data$Is.Ref._ == "0" &
+              data$Gasconfiguration == "N2", "d.15N.14N"]
+GA1.avg <- mean(GA1)
+
+#Grab all the GA2 data
+GA2 <- data[data$Identifier.1 %in% c("GA2_QTY_02", 
+                                     "GA2_STD_01", 
+                                     "GA2_STD_02", 
+                                     "GA2_STD_03", 
+                                     "GA2_STD_04") &
+              data$Is.Ref._ == "0" &
+              data$Gasconfiguration == "N2", "d.15N.14N"]
+
+GA2.avg <- mean(GA2)
+
+#Grab all the SAL data
+PCH <- data[data$Identifier.1 %in% c("PCH_STD_01", 
+                                     "PCH_STD_02", 
+                                     "PCH_STD_03", 
+                                     "PCH_STD_04") &
+              data$Is.Ref._ == "0" &
+              data$Gasconfiguration == "N2", "d.15N.14N"]
+
+PCH.avg <- mean(PCH)
+
+#Find offset values
+GA1.off <- GA1.avg- GA1.ref
+GA2.off <- GA2.avg - GA2.ref
+PCH.off <- PCH.avg - PCH.ref
+
+#Plot 
+offset <- c(GA1.off, GA2.off, PCH.off)
+measured <- c(GA1.avg, GA2.avg, PCH.avg)
+
+plot(x = measured, y = offset)
+
+#Linear regression
+lm.1 <- lm(offset ~ measured)
+summary(lm.1)
