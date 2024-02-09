d7018642a7cc572c50b1e1a93d1917c6fd0a68f6
Grace Henry <henryn98@uw.edu>
2024-02-09 09:02:01 -0800

    edited

--- code/Correct_to_VPDB.R
+++ code/Correct_to_VPDB.R
@@ -11,4 +11,30 @@ setwd("~/Documents/GitHub/CSIA_lab_work/data/SIA results")
 #Load in data
 data <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/SIA results/230914_Jameson_10B-AqPlants.csv")
 
+#True values referenced to VPDB
+GA1 <- 
+GA2 <- 
+PCH <- 
+
+#Grab all GA1 data
+GA1 <- data[data$Identifier.1 %in% c("GA1_QTY_2", 
+                                     "GA1_STD_01", 
+                                     "GA1_STD_02", 
+                                     "GA1_STD_03", 
+                                     "GA1_STD_04"), ]
+
+#Grab all the GA2 data
+GA2 <- data[data$Identifier.1 %in% c("GA2_QTY_02", 
+                                     "GA2_STD_01", 
+                                     "GA2_STD_02", 
+                                     "GA2_STD_03", 
+                                     "GA2_STD_04"), ]
+
+#Grab all the SAL data
+PCH <- data[data$Identifier.1 %in% c("PCH_STD_01", 
+                                     "PCH_STD_02", 
+                                     "PCH_STD_03", 
+                                     "PCH_STD_04"), ]
+
+
 
