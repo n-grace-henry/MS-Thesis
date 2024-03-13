61becf3268de007b3c6a050071cc29f5da1990ad
Grace Henry <henryn98@uw.edu>
2024-03-13 11:45:18 -0700

    trying to figure out how best to make the time series objects

--- code/ChangePointAnalysis.R
+++ code/ChangePointAnalysis.R
@@ -7,6 +7,35 @@ ggplot(data, aes(x = Year,
                  y = PHE.mean)) +
   geom_point(size = 3, alpha = 0.7)
 
+# Getting the data in a format that will work to create a time series object
+# Convert the "Year" column to a factor to ensure correct ordering
+data$Year <- factor(data$Year, levels = rev(unique(data$Year)))
+
+# Pivot the data to wide format to create separate columns for each river and age combination
+library(tidyr)
+data_wide <- spread(data, System, PHE.mean)
+
+# Create separate time series objects for each river and age combination
+ts_river1_age2 <- ts(data_wide$age[data_wide$River == "River1" & data_wide$age == 2], 
+                     start = 2022, frequency = 1)
+ts_river1_age3 <- ts(data_wide$age[data_wide$River == "River1" & data_wide$age == 3], 
+                     start = 2022, frequency = 1)
+ts_river2_age2 <- ts(data_wide$age[data_wide$River == "River2" & data_wide$age == 2], 
+                     start = 2022, frequency = 1)
+ts_river2_age3 <- ts(data_wide$age[data_wide$River == "River2" & data_wide$age == 3], 
+                     start = 2022, frequency = 1)
+ts_river3_age2 <- ts(data_wide$age[data_wide$River == "River3" & data_wide$age == 2], 
+                     start = 2022, frequency = 1)
+ts_river3_age3 <- ts(data_wide$age[data_wide$River == "River3" & data_wide$age == 3], 
+                     start = 2022, frequency = 1)
+
+
+
+
+
+
+
+
 #load packages
 library(changepoint)
 
