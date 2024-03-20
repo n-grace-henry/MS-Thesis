4599f24fbbf74a31cf35c166ebea81efc6f64b02
Grace Henry <henryn98@uw.edu>
2024-03-20 09:37:12 -0700

    separatng data for clean ts analysis

--- code/ChangePointAnalysis.R
+++ code/ChangePointAnalysis.R
@@ -13,20 +13,51 @@ data$Year <- factor(data$Year, levels = rev(unique(data$Year)))
 
 # Pivot the data to wide format to create separate columns for each river and age combination
 library(tidyr)
-data_wide <- spread(data, System, PHE.mean)
+data_wide <- pivot_wider(data, names_from = System, values_from = PHE.mean)
+
+# Create data frames for each of the river systems 
+Eg <- data_wide[, c("Egegik", "Year",
+                "Trophic.Position", "Age")]
+Kvi <- data_wide[, c("Kvichak", "Year",
+                    "Trophic.Position", "Age")]
+Wood <- data_wide[, c("Wood", "Year",
+                      "Trophic.Position", "Age")]
+
+
+# Get rid of years from other systems
+Eg <- Eg[!is.na(Eg$Egegik), ]
+Kvi <- Kvi[!is.na(Kvi$Kvichak), ]
+Wood <- Wood[!is.na(Wood$Wood), ]
+
+# Add NAs where they are missing
+newrow <- c(NA, "2019", NA, "3")
+insertRow <- 
+
+existingDF <- as.data.frame(matrix(seq(20),nrow=5,ncol=4))
+r <- 3
+newrow <- seq(4)
+insertRow <- function(existingDF, newrow, r) {
+  existingDF[seq(r+1,nrow(existingDF)+1),] <- existingDF[seq(r,nrow(existingDF)),]
+  existingDF[r,] <- newrow
+  existingDF
+}
+
+# Separate by Age class
+Eg_2 <- Eg[Eg$Age == "2",]
+Eg_3 <- Eg[Eg$Age == "3",]
 
 # Create separate time series objects for each river and age combination
-ts_river1_age2 <- ts(data_wide$age[data_wide$River == "River1" & data_wide$age == 2], 
+ts_river1_age2 <- ts(data_wide$Egegik[data_wide$Age == 2], 
                      start = 2022, frequency = 1)
-ts_river1_age3 <- ts(data_wide$age[data_wide$River == "River1" & data_wide$age == 3], 
+ts_river1_age3 <- ts(data_wide$River1[data_wide$age == 3], 
                      start = 2022, frequency = 1)
-ts_river2_age2 <- ts(data_wide$age[data_wide$River == "River2" & data_wide$age == 2], 
+ts_river2_age2 <- ts(data_wide$River2[data_wide$age == 2], 
                      start = 2022, frequency = 1)
-ts_river2_age3 <- ts(data_wide$age[data_wide$River == "River2" & data_wide$age == 3], 
+ts_river2_age3 <- ts(data_wide$River2[data_wide$age == 3], 
                      start = 2022, frequency = 1)
-ts_river3_age2 <- ts(data_wide$age[data_wide$River == "River3" & data_wide$age == 2], 
+ts_river3_age2 <- ts(data_wide$River3[data_wide$age == 2], 
                      start = 2022, frequency = 1)
-ts_river3_age3 <- ts(data_wide$age[data_wide$River == "River3" & data_wide$age == 3], 
+ts_river3_age3 <- ts(data_wide$River3[data_wide$age == 3], 
                      start = 2022, frequency = 1)
 
 
