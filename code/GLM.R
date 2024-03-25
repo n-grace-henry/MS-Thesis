81639cc7bd88db5a08783c6c33e62bca3533b8c9
Grace Henry <henryn98@uw.edu>
2024-03-25 13:44:46 -0700

    new file

---
+++ code/GLM.R
@@ -0,0 +1,16 @@
+setwd("~/Documents/GitHub/CSIA_lab_work/data")
+
+#Load Packages
+library(dplyr)
+library(ggplot2)
+
+#Load Data
+data <- read.csv(file = "final/data.csv")
+PDO <- read.csv(file = "Environmental/PDO.csv")
+NPGO <- read.csv(file = "Environmental/NPGO.csv")
+
+#Get per year average of both data frames 
+library(dplyr)
+avg_data_all <- data %>%
+  group_by(Year) %>%
+  summarise(avg_PHE = mean(PHE.mean, na.rm = TRUE))
