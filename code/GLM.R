4dc561a080af0a5e36e889843bac4852baaad7a5
Grace Henry <henryn98@uw.edu>
2024-03-27 15:55:14 -0700

    edited

--- code/GLM.R
+++ code/GLM.R
@@ -13,11 +13,23 @@ NPGO <- read.csv(file = "Environmental/NPGO.csv")
 library(dplyr)
 
 #PDO
-PDO_annual <- matrix(nrow = 2, ncol = 2)
-for(i in 1:length(PDO$Year))
-  
-  
+PDO_annual <- matrix(nrow = length(PDO$Year), ncol = 2)
+for(i in 1:length(PDO$Year)){
+  PDO_annual[i,1] <- PDO$Year[i]
+  PDO_annual[i,2] <- mean(as.numeric(PDO[i, 2:13]))
+}
+plot(PDO_annual,
+     type = "l")
+ts.PDO <- ts(PDO_annual[,2], 
+             start = PDO_annual[1,1],
+             end = PDO_annual[169,1],
+             frequency = 1)
+plot(x = ts.PDO)
+cpt <- cpt.mean(as.vector(scale(ts.PDO)), method = "PELT")
+plot(cpt, cpt.col = "blue")
+summary(cpt)
 
-PDO_annual <- PDO %>%
-  group_by(Year) %>%
-  summarise(avg = mean(PHE.mean, na.rm = TRUE))
+
+#NPGO
+
+
