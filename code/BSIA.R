ee23b85fcc75fd62e8d13495ba901f5fc6215e72
Grace Henry <henryn98@uw.edu>
2024-04-10 14:12:53 -0700

    fixed one excel sheet

--- code/BSIA.R
+++ code/BSIA.R
@@ -9,5 +9,9 @@ none
 HEEL::EA.NACHO("221116_GHenry_EA.csv")
 none
 
-a <- read.csv(file = "~/Documents/Grad School /SIA/HEEL_NACHO_program/Data/220725_LElmstrom_EA.csv")
-HEEL::EA.NACHO("~/Documents/Grad School /SIA/HEEL_NACHO_program/Data/230809_GHenry_EA.csv")
+a <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/EA results/cleaned/240403_GHenry_20240403.csv")
+HEEL::EA.NACHO("~/Documents/GitHub/CSIA_lab_work/data/EA results/cleaned/240403_GHenry_20240403.csv")
+
+
+HEEL::EA.NACHO("~/Desktop/220909_SSabikunnahar_EA.csv")
+both
