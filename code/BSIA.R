8b93016217214163dff5edfcffa83b650c74ee8d
Grace Henry <henryn98@uw.edu>
2024-04-10 14:26:40 -0700

    cleaning EA results

--- code/BSIA.R
+++ code/BSIA.R
@@ -3,15 +3,11 @@
 devtools::install_github("gholtgrieve/HEEL")
 rm(list=ls())
 library(HEEL)
-HEEL::EA.NACHO("~/Documents/GitHub/CSIA_lab_work/data/EA results/cleaned/240403_GHenry_20240403.csv")
-none
-
-HEEL::EA.NACHO("221116_GHenry_EA.csv")
-none
-
-a <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/EA results/cleaned/240403_GHenry_20240403.csv")
-HEEL::EA.NACHO("~/Documents/GitHub/CSIA_lab_work/data/EA results/cleaned/240403_GHenry_20240403.csv")
-
-
-HEEL::EA.NACHO("~/Desktop/220909_SSabikunnahar_EA.csv")
+HEEL::EA.NACHO("~/Documents/GitHub/CSIA_lab_work/data/EA results/cleaned/240329_GHenry_20240329.csv")
 both
+
+
+
+
+
+
