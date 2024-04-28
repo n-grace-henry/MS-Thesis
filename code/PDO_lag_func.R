8a320f2d4153533000011b0911da9e72f6d0c541
Grace Henry <henryn98@uw.edu>
2024-04-28 10:55:19 -0700

    edited

--- code/PDO_lag_func.R
+++ code/PDO_lag_func.R
@@ -29,8 +29,8 @@ ret_mo <- 7
 lag <- 0
 months <- 12
 
-
-PDO_lag <- function(ret_yr, ret_mo, lag, months){
+#must have PDO and PDO_long loaded in global environment
+PDO_lag <- function(ret_yr, ret_mo, lag, months, yrs, name){
   #create empty data frame to fill with values
   PDO_new <- as.data.frame(matrix(nrow = length(PDO$Year), ncol = 2))
   names(PDO_new) <- c("Year","PDO")
@@ -38,16 +38,22 @@ PDO_lag <- function(ret_yr, ret_mo, lag, months){
   #loop to get applicable values for each year
   for(i in 1:length(PDO$Year)){
     PDO_new[i,1] <- PDO$Year[i+1]
-    PDO_new[i,2] <- mean(PDO_long[PDO_long$Year %in% c(PDO$Year[i], PDO$Year[i+1]) & 
-                                    (PDO_long$Year == PDO$Year[i] & PDO_long$month_number >= 7 |
-                                       PDO_long$Year == PDO$Year[i+1] & PDO_long$month_number <= 7), "Value"])
+    PDO_new[i,2] <- mean(PDO_long[PDO_long$Year %in% c(PDO$Year[i], PDO$Year[i+yrs]) & 
+                                    (PDO_long$Year == PDO$Year[i] & PDO_long$month_number >= ret_mo |
+                                       PDO_long$Year == PDO$Year[i+yrs] & PDO_long$month_number <= ret_mo), "Value"])
     
   }
-  
+  name <- PDO_new
+  return(name)
 }
 
+#everything between two points
+PDO_long_subset <- PDO_long[PDO_long$Year >= 1952 & 
+                              (PDO_long$Year < 1955 | (PDO_long$Year == 1955 & PDO_long$month_number <= 3)), ]
+
+
 #one year of data from July-July
-#if return year is 1914, and we want one yr from return, then we go back to july 1913
+#if return year is 1914, and we want one yr from return, then we go back to July 1913
 PDO_new <- as.data.frame(matrix(nrow = length(PDO$Year), ncol = 2))
 names(PDO_new) <- c("Year","PDO")
 for(i in 1:length(PDO$Year)){
