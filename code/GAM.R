4d0482d9ba1d29b673235501136286b259811259
Grace Henry <henryn98@uw.edu>
2024-05-20 09:52:32 -0700

    looking at random effects of system

--- code/GAM.R
+++ code/GAM.R
@@ -1,7 +1,9 @@
 # Load packages
+library(tidyr)
 library(dplyr)
 library(mgcv)
 library(readr)
+library(effects)
 
 # Read in data
 data <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/final/all_correct_final.csv")
@@ -50,5 +52,18 @@ ggplot(data, aes(x = Year, y = GLU.mean, col = System)) +
   geom_smooth(method = "gam", formula = y ~ s(x), col = "red") +
   theme_minimal()
 
-#Factor effect of system, is it significant 
-# outside smooth term is one trend through time 
+# Effect of system on Trophic Position
+data$Year <- as.numeric(data$Year)
+data$System <- as.factor(data$System) #change data structure
+
+sys_gam_factor <- gam(Trophic.Position ~ s(Year) + System, data = data)
+summary(sys_gam_factor) #assumes the effect of system is constant over time, different intercepts 
+
+#sys_gam <- gam(Trophic.Position ~ s(Year, System), data = data)
+#summary(sys_gam) this doesn't work
+
+sys_gam_by <- gam(Trophic.Position ~ s(Year, by = System) + System, data = data)
+summary(sys_gam_by)
+
+# AIC to compare models
+AIC(sys_gam_factor, sys_gam_by, gam_model_trophic)
