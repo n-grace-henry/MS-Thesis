c557329d05330d1a520ad034b81d97e71def989f
Grace Henry <henryn98@uw.edu>
2024-06-03 10:59:46 -0700

    taking state space model notes

---
+++ code/State_Space.R
@@ -0,0 +1,12 @@
+# Load packages
+library(stats)
+library(MARSS)
+library(forecast)
+library(datasets)
+
+
+# Notes 
+# We take observations of some underlying stochastic process and data imperfectly 
+# measures hidden process
+
+
