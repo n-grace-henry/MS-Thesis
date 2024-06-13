3990f58a469dcdbf0fd4835527c909912386e539
Grace Henry <henryn98@uw.edu>
2024-06-13 14:02:00 -0700

    changed format of time series objects for each system/age

--- code/Univariate_Baseline.R
+++ code/Univariate_Baseline.R
@@ -29,13 +29,88 @@ Kvichak.2 <- merged_df %>% filter(System == "Kvichak") %>% filter(Age == 2)
 Kvichak.3 <- merged_df %>% filter(System == "Kvichak") %>% filter(Age == 3)
 
 # Convert to time series objects
-Wood.2_ts <- ts(Wood.2$PHE.mean, start = 1965, frequency = 1)
-Wood.3_ts <- ts(Wood.3$PHE.mean, start = 1965, frequency = 1)
-Egegik.2_ts <- ts(Egegik.2$PHE.mean, start = 1965, frequency = 1)
-Egegik.3_ts <- ts(Egegik.3$PHE.mean, start = 1965, frequency = 1)
-Kvichak.2_ts <- ts(Kvichak.2$PHE.mean, start = 1965, frequency = 1)
-Kvichak.3_ts <- ts(Kvichak.3$PHE.mean, start = 1965, frequency = 1)
+Wood2.data <- c(4.280390, NA, NA, 3.345011, NA, NA, 6.680199, NA, NA,
+                              4.416843, NA, NA, 4.794477, NA, NA, 5.024736, NA, NA,
+                              NA, 6.662367, NA, 3.473617, NA, NA, 2.553377, NA, NA,
+                              6.817998, NA, NA, 3.996405, NA, NA, 6.824048, NA, NA,
+                              3.137087, NA, NA, 3.388897, NA, NA, 3.326144, NA, NA,
+                              4.558063, NA, NA, 4.453277, NA, NA, 3.815537, NA, NA,
+                              5.437850, NA, NA, 4.780483)
+Wood2_ts <- ts(Wood2.data, start = 1965, frequency = 1)
+
+Wood3.data <- c(NA, NA, NA, 3.629854, NA, NA, 2.452854, NA, NA,
+                              NA, NA, NA, 1.397096, NA, NA, NA, NA, NA,
+                              NA, NA, NA, 6.431029, NA, NA, 3.989506, NA, NA,
+                              3.343838, NA, NA, 4.535749, NA, NA, NA, NA, NA,
+                              6.479993, NA, NA, 7.014444, NA, NA, NA, NA, NA,
+                              NA, NA, NA, 4.380171, NA, NA, 5.011158, NA, NA,
+                              NA, NA, NA, 4.213152)
+Wood3_ts <- ts(Wood3.data, start = 1965, frequency = 1)
+
+Egegik2.data <- c(2.516461, NA, 1.935622, NA, NA, NA, 4.193070, NA, NA,
+                   3.978130, NA, NA, 5.100270, NA, NA, NA, NA, NA,
+                   NA, NA, NA, 7.048355, NA, NA, 5.921672, NA, NA,
+                   3.707803, NA, NA, 4.765402, NA, NA, 2.385568, NA, NA,
+                   5.395242, NA, NA, 4.403601, NA, NA, 6.318120, NA, NA,
+                   4.104897, NA, NA, 1.579084, NA, NA, 3.183578, NA, NA,
+                   4.675207, NA, NA, 2.289712)
+Egegik2_ts <- ts(Egegik2.data, start = 1965, frequency = 1)
+
+Egegik3.data <- c(NA, NA, 3.900713, NA, NA, NA, 3.865586, NA, NA,
+                   NA, NA, NA, 1.412124, NA, NA, NA, NA, NA,
+                   NA, NA, NA, NA, NA, NA, 2.500479, NA, NA,
+                   NA, NA, NA, 8.215039, NA, NA, 5.709907, NA, NA,
+                   5.060625, NA, NA, 2.409714, NA, NA, 2.346770, NA, NA,
+                   2.219991, NA, NA, 3.351748, NA, NA, NA, NA, NA,
+                   NA, NA, NA, 1.484118)
+Egegik3_ts <- ts(Egegik3.data, start = 1965, frequency = 1)
+
+Kvichak2.data <- c(4.249013, NA, NA, 1.855945, NA, NA, 3.255309, NA, NA,
+                    3.564758, NA, NA, 4.489917, NA, NA, 3.403307, NA, NA,
+                    3.956233, NA, NA, NA, NA, NA, 5.375161, NA, NA,
+                    NA, 6.450664, NA, 3.342840, NA, NA, 6.973176, NA, NA,
+                    6.354919, NA, NA, 8.714515, NA, NA, 6.008137, NA, NA,
+                    3.284504, NA, NA, 5.925795, NA, NA, 3.762834, NA, NA,
+                    5.237393, NA, NA, 4.866149)
+Kvichak2_ts <- ts(Kvichak2.data, start = 1965, frequency = 1)
+
+Kvichak3.data <- c(NA, NA, NA, NA, NA, NA, 2.855826, NA, NA,
+                    NA, NA, NA, NA, NA, NA, NA, NA, NA,
+                    NA, NA, NA, NA, NA, NA, NA, NA, NA,
+                    NA, NA, NA, NA, NA, NA, NA, NA, NA,
+                    6.289370, NA, NA, 3.579844, NA, NA,
+                    NA, NA, NA, NA, NA, NA, NA, 1.136779,
+                    NA, NA, NA, NA, NA, 4.523967, NA, NA,
+                    3.672443) 
+Kvichak3_ts <- ts(Kvichak3.data, start = 1965, frequency = 1)
+
+# Plot the time series object
+plot(Wood2_ts, type = "o", col = "blue", xlab = "Year", ylab = "PHE.mean", main = "Time Series Plot")
+
 
 #### Model Univariate State-Space Baseline ####
+
+# Wood age 2
+mod.list <- list(
+  U = matrix("u"),
+  x0 = matrix("x0"),
+  B = matrix(1),
+  Q = matrix("q"),
+  Z = matrix(1),
+  A = matrix(0),
+  R = matrix("r"), 
+  tinitx = 0
+)
+fit <- MARSS(Wood.2_ts, model = mod.list)
+plot(Wood.2_ts)
+lines(1965:2022, fit$states[1,], col = "red")
+
+ts.plot(Wood2_ts)
+
+# Create the time series object
+ts_object <- ts(ts_data, start = 1965, end = 2022, frequency = 1)
+
+# Plot the time series object
+plot(Wood2_ts, type = "o", col = "blue", xlab = "Year", ylab = "PHE.mean", main = "Time Series Plot")
 
 
