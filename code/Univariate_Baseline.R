872ca38613c318e52dea67eee2c7b739cf4c85bd
Grace Henry <henryn98@uw.edu>
2024-06-13 13:12:51 -0700

    done setting up time series objects

--- code/Univariate_Baseline.R
+++ code/Univariate_Baseline.R
@@ -15,26 +15,27 @@ data <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/final/all_correct
 years <- seq(1965, 2022, by = 1)
 
 # Create a complete data frame for each river system
-complete_df <- expand.grid(Year = years, System = unique(data$System))
+complete_df <- expand.grid(Year = years, System = unique(data$System), Age = unique(data$Age))
 
 # Merge with the original data to insert NA values for missing data
-merged_df <- left_join(complete_df, data, by = c("Year", "System"))
+merged_df <- left_join(complete_df, data, by = c("Year", "System", "Age"))
 
-# Split the data by river system
-Wood <- merged_df %>% filter(System == "Wood")
-Egegik <- merged_df %>% filter(System == "Egegik")
-Kvichak <- merged_df %>% filter(System == "Kvichak")
+# Split the data by river system and age class
+Wood.2 <- merged_df %>% filter(System == "Wood") %>% filter(Age == 2)
+Wood.3 <- merged_df %>% filter(System == "Wood") %>% filter(Age == 3)
+Egegik.2 <- merged_df %>% filter(System == "Egegik") %>% filter(Age == 2)
+Egegik.3 <- merged_df %>% filter(System == "Egegik") %>% filter(Age == 3)
+Kvichak.2 <- merged_df %>% filter(System == "Kvichak") %>% filter(Age == 2)
+Kvichak.3 <- merged_df %>% filter(System == "Kvichak") %>% filter(Age == 3)
 
 # Convert to time series objects
-Wood_ts <- ts(river1_df$PHE.mean, start = 1965, frequency = 1)
-Egegik_ts <- ts(river2_df$PHE.mean, start = 1965, frequency = 1)
-Kvichak_ts <- ts(river3_df$PHE.mean, start = 1965, frequency = 1)
+Wood.2_ts <- ts(Wood.2$PHE.mean, start = 1965, frequency = 1)
+Wood.3_ts <- ts(Wood.3$PHE.mean, start = 1965, frequency = 1)
+Egegik.2_ts <- ts(Egegik.2$PHE.mean, start = 1965, frequency = 1)
+Egegik.3_ts <- ts(Egegik.3$PHE.mean, start = 1965, frequency = 1)
+Kvichak.2_ts <- ts(Kvichak.2$PHE.mean, start = 1965, frequency = 1)
+Kvichak.3_ts <- ts(Kvichak.3$PHE.mean, start = 1965, frequency = 1)
 
-# Print the time series objects to verify
-print(river1_ts)
-print(river2_ts)
-print(river3_ts)
+#### Model Univariate State-Space Baseline ####
 
-
-phe_ts <- ts(data$PHE.mean, start = 1965, end = 2022, frequency = 1)
 
