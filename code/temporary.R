6718ef6cc860e3c051a0424edfc1c7ae65c9194d
Grace Henry <henryn98@uw.edu>
2024-06-19 15:59:08 -0700

    making a testing script that I will eventually delete

---
+++ code/temporary.R
@@ -0,0 +1,15 @@
+# Load packages 
+library(stats)
+library(MARSS)
+library(forecast)
+library(datasets)
+library(ggplot2)
+library(dplyr)
+
+# Load data 
+data <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/final/mass_correct.csv")
+
+# Clean 
+data <- data[,-c(1:4)]
+
+# Put into 9 by t matrix for MARSS
