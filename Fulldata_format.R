82562045ec5bcf8b7c462f38f1b2f65007fbcbdd
Grace Henry <henryn98@uw.edu>
2024-07-11 18:19:43 -0800

    setting up formatting script

--- Fulldata_format.R
+++ Fulldata_format.R
@@ -1,2 +1,7 @@
+# Read in data
 data.full <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/final/full.csv")
 
+# Delete replicates and triplicates??
+
+
+# Format: three points per year, every year represented
