179abb04ae0d9a9eb3e8f5e705b44af7332e9ab8
Grace Henry <henryn98@uw.edu>
2024-07-12 17:15:23 -0800

    copied code to create full data frame

--- code/Fulldata_format.R
+++ code/Fulldata_format.R
@@ -19,5 +19,20 @@ index.PHE <- which(substr(data.PHE$ID1, 8, 8) != "")
 data.GLU <- data.GLU[-index.GLU,]
 data.PHE <- data.PHE[-index.PHE,]
 
-#
+# Create full data set 
+
+
+
+#copied from other script 
+# Create a complete sequence of years
+years <- seq(1965, 2022, by = 1)
+
+# Create a complete data frame for each river system
+complete_df <- expand.grid(Year = years, System = unique(data$System), Age = unique(data$Age))
+
+# Merge with the original data to insert NA values for missing data
+merged_df <- left_join(complete_df, data, by = c("Year", "System", "Age"))
+merged_df <- merged_df[order(merged_df$Year, merged_df$Age, merged_df$System), ]
+
+
 
