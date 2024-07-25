4ada107cfaefc74da135c5e23ee0acf534834a6e
Grace Henry <henryn98@uw.edu>
2024-07-24 21:16:09 -0800

    working on PDO averaging

--- code/PDO_lags.R
+++ code/PDO_lags.R
@@ -41,6 +41,7 @@ names(PDO_avg) <- c("Year","PDO")
 
 
 
+
 #### Linear Model for 6 months before return ####
 #January - July of return year
 #Biologically this model doesn't make a lot of sense 
@@ -50,16 +51,6 @@ for(i in 1:length(PDO$Year)){
   PDO_six_month[i,1] <- PDO$Year[i]
   PDO_six_month[i,2] <- mean(as.numeric(PDO[i,c(2:7)]))
 } 
-
-#Linear model 
-merged <- merge(avg_phe, PDO_six_month, by = "Year", all = TRUE)
-names(merged) <- c("Year", "PHE", "PDO")
-merged <- na.omit(merged)
-
-model2 <- lm(PHE ~ PDO, data = merged)
-summary(model2)
-plot(x = merged$PDO,
-     y = merged$PHE)
 
 
 
