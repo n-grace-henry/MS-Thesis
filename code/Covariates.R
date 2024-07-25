84675d560732de3490e0311d74590a589269b02e
Grace Henry <henryn98@uw.edu>
2024-07-24 17:34:39 -0800

    working on covariates script

---
+++ code/Covariates.R
@@ -0,0 +1,16 @@
+# Load packages 
+library(tidyverse)
+
+# Load state data 
+states <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/final/states2.csv")
+
+# Load environmental data 
+
+
+# Plot baseline data for all systems 
+
+
+# Plot GLU data for all systems 
+
+
+# Plot trophic position for all systems 
