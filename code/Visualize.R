f4504b46684cd602d4077183e8ed8da453766a5f
Grace Henry <henryn98@uw.edu>
2024-08-01 11:07:08 -0800

    saved plots of all data

--- code/Visualize.R
+++ code/Visualize.R
@@ -40,10 +40,10 @@ PHE.all <- ggplot(data = data, aes(x = Year, y = PHE.mean, color = System)) +
        x = "Year",
        y = "PHE d15N") +
   theme(axis.title = element_text(size = 15),
-        plot.title = element_text(size=16)) +
+        plot.title = element_text(size=16)) #+
  # geom_vline(xintercept=1977, linetype ="dashed") +
   #geom_vline(xintercept=1998, linetype ="dashed") +
-  geom_smooth(aes(group=1)) 
+  #geom_smooth(aes(group=1)) 
 #+facet_grid(. ~ Age, labeller = labeller(Age = c("2" = "Age 2", "3" = "Age 3")))
 
 PHE.all <- ggplot(data = data, aes(x = Year, y = PHE.mean, color = System)) +
@@ -99,10 +99,10 @@ GLU.all <- ggplot(data = data, aes(x = Year, y = GLU.mean, color = System)) +
        x = "Year",
        y = "GLU d15N") +
   theme(axis.title = element_text(size = 15),
-        plot.title = element_text(size=16)) +
+        plot.title = element_text(size=16)) #+
   #geom_vline(xintercept=1977, linetype ="dashed") +
   #geom_vline(xintercept=1998, linetype ="dashed") +
-  geom_smooth(aes(group=1)) 
+  #geom_smooth(aes(group=1)) 
 #+facet_grid(. ~ Age, labeller = labeller(Age = c("2" = "Age 2", "3" = "Age 3")))
 
 GLU.all <- ggplot(data = data, aes(x = Year, y = GLU.mean, color = System)) +
