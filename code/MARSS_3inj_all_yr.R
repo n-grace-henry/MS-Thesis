2dd27cc3b2d15eab4793e01a764bae50e434c74b
Grace Henry <henryn98@uw.edu>
2024-08-10 14:09:17 -0800

    changing some file names

--- code/MARSS_3inj_all_yr.R
+++ code/MARSS_3inj_all_yr.R
@@ -9,3 +9,7 @@ library(tidyverse)
 
 # Load data 
 data.full <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/final/full.csv")
+
+# Subset data for age 2 and only PHE
+PHE <- data.full[data.full$Age == "2" &
+                   data.full$AAID == "PHE", c("Year", "System", "Age", "adj", "ID1", "Rep")]
