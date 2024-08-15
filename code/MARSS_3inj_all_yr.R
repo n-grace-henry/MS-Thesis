35c810f2cca6ff584370fc0718266560abd26deb
Grace Henry <henryn98@uw.edu>
2024-08-15 11:45:30 -0800

    got both GLU and PHE properly formatted for model

--- code/MARSS_3inj_all_yr.R
+++ code/MARSS_3inj_all_yr.R
@@ -17,33 +17,74 @@ PHE <- data.full[data.full$Age == "2" &
 # Subset Wood PHE
 PHE.W <- PHE[PHE$System == "Wood", c("Year", "adj", "ID1", "Rep")]
 
-# Full years to represent in data
-full_years <- seq(1965, 2022, by = 1)
+# Subset Kvichak PHE 
+PHE.K <- PHE[PHE$System == "Kvichak", c("Year", "adj", "ID1", "Rep")]
 
-# Format data: three injections per year, every year represented
-long.all.yr <- PHE.W %>%
-  arrange(Year, ID1, Rep) %>%  # Arrange data by Year, ID, and Rep
-  filter(!Rep %in% c("R", "a")) %>%  # Filter out unwanted replicates
-  group_by(Year) %>%  # Group by Year
-  mutate(SampleNumber = as.character(row_number())) %>%  # Assign and convert unique sample numbers to characters
-  select(-ID1, -Rep) %>%  # Remove columns ID and Rep
-  ungroup() %>%  # Ungroup to avoid issues with expand.grid
-  right_join(
-    expand.grid(
-      Year = full_years,
-      SampleNumber = c("1", "2", "3")
-    ),
-    by = c("Year", "SampleNumber")
-  ) %>%
-  arrange(Year, SampleNumber)
+# Subset Egegik PHE
+PHE.E <- PHE[PHE$System == "Egegik", c("Year", "adj", "ID1", "Rep")]
 
-# Convert to wide format
-wide <- long.all.yr %>%
-  pivot_wider(names_from = SampleNumber, values_from = adj, names_prefix = "Inj")
+# Subset data for age 2 and only PHE
+GLU <- data.full[data.full$Age == "2" &
+                   data.full$AAID == "GLU", c("Year", "System", "Age", "adj", "ID1", "Rep")]
 
-# Transpose wide format
-wide.t <- t(wide)
-wide.t <- wide.t[-1,]
+# Subset Wood GLU
+GLU.W <- GLU[GLU$System == "Wood", c("Year", "adj", "ID1", "Rep")]
+
+# Subset Kvichak GLU
+GLU.K <- GLU[GLU$System == "Kvichak", c("Year", "adj", "ID1", "Rep")]
+
+# Subset Egegik GLU
+GLU.E <- GLU[GLU$System == "Egegik", c("Year", "adj", "ID1", "Rep")]
+
+# Function to get all data to wide format 
+wide <- function(data){
+  # Full years to represent in data
+  full_years <- seq(1965, 2022, by = 1)
+  
+  # Format data: three injections per year, every year represented
+  long <- data %>%
+    arrange(Year, ID1, Rep) %>%  # Arrange data by Year, ID, and Rep
+    filter(!Rep %in% c("R", "a")) %>%  # Filter out unwanted replicates
+    group_by(Year) %>%  # Group by Year
+    mutate(SampleNumber = as.character(row_number())) %>%  # Assign and convert unique sample numbers to characters
+    select(-ID1, -Rep) %>%  # Remove columns ID and Rep
+    ungroup() %>%  # Ungroup to avoid issues with expand.grid
+    right_join(
+      expand.grid(
+        Year = full_years,
+        SampleNumber = c("1", "2", "3")
+      ),
+      by = c("Year", "SampleNumber")
+    ) %>%
+    arrange(Year, SampleNumber)
+  
+  # Convert to wide format
+  wide <- long %>%
+    pivot_wider(names_from = SampleNumber, values_from = adj, names_prefix = "Inj")
+  
+  # Transpose
+  wide.t <- t(wide)
+  wide.t <- wide.t[-1,]
+  
+  # Return data
+  return(wide.t)
+}
+
+# Convert all PHE systems to wide using function
+wide.PHE.W <- wide(PHE.W)
+wide.PHE.K <- wide(PHE.K)
+wide.PHE.E <- wide(PHE.E)
+
+# Convert all GLU systmes to wide using function
+wide.GLU.W <- wide(GLU.W)
+wide.GLU.K <- wide(GLU.K)
+wide.GLU.E <- wide(GLU.E)
+
+# Combine all PHE systems
+all.PHE <- rbind(wide.PHE.W, wide.PHE.K, wide.PHE.E)
+
+# Combine all GLU systems
+all.GLU <- rbind(wide.GLU.W, wide.GLU.K, wide.GLU.E)
 
 # Model across all years 
 mod.list.1 <- list(
