67ba786246e7b23de055cfff3e57cb7138284cec
Grace Henry <henryn98@uw.edu>
2024-08-22 12:27:52 -0700

    working on 6 injection function and analysis

--- code/MARSS_6inj.R
+++ code/MARSS_6inj.R
@@ -44,6 +44,42 @@ wide <- long.all.yr %>%
 wide.t <- t(wide)
 wide.t <- wide.t[-1,]
 
+# Create function to format data correctly 
+format <- function(data) {
+  # Full years to represent in data
+  full_years <- seq(1965, 2022, by = 1)
+  
+  # Format data: three six per year, every year represented
+  long.all.yr <- PHE.W %>%
+    arrange(Year, ID1, Rep) %>%  # Arrange data by Year, ID, and Rep
+    group_by(Year) %>%  # Group by Year
+    mutate(SampleNumber = as.character(row_number())) %>%  # Assign and convert unique sample numbers to characters
+    select(-ID1, -Rep) %>%  # Remove columns ID and Rep
+    ungroup() %>%  # Ungroup to avoid issues with expand.grid
+    right_join(
+      expand.grid(
+        Year = full_years,
+        SampleNumber = c("1", "2", "3", "4", "5", "6")
+      ),
+      by = c("Year", "SampleNumber")
+    ) %>%
+    arrange(Year, SampleNumber)
+  
+  # Convert to wide format
+  wide <- long.all.yr %>%
+    pivot_wider(names_from = SampleNumber, values_from = adj, names_prefix = "Inj")
+  
+  # Transpose wide format
+  wide.t <- t(wide)
+  wide.t <- wide.t[-1,]
+  
+  
+}
+
+
+
+
+
 # Run model
 mod.list.1 <- list(
   B = matrix(1),           # State transition matrix
