349558d7f2966a82537723d86ec92743dda144e0
Grace Henry <henryn98@uw.edu>
2024-10-02 12:07:18 -0700

    starting size at age analysis script

---
+++ code/SizeAtAge.R
@@ -0,0 +1,13 @@
+# Load libraries 
+library(ggplot2)
+library(dplyr)
+
+# Load data
+size <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/environmental/SaA_covariates_2024.csv")
+str(size)
+
+# Delete last row
+size <- size[!size$Year %in% c(2023, 2024),]
+
+# Delete last two columns
+size <- size[, -c(7, 8)]
