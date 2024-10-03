d5c1bd5088a969162725289f7193ec3b859c51bc
Grace Henry <henryn98@uw.edu>
2024-10-03 13:09:47 -0700

    added ohlberger data to repo

--- code/SizeAtAge.R
+++ code/SizeAtAge.R
@@ -5,6 +5,7 @@ library(dplyr)
 # Load data
 size <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/environmental/SaA_covariates_2024.csv")
 str(size)
+data <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/environmental/Ohlberger.csv")
 
 # Delete last row
 size <- size[!size$Year %in% c(2023, 2024),]
