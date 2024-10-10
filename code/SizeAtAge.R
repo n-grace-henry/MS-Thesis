feca10bcae2c5177a1bde00161816f7a92e34243
Grace Henry <henryn98@uw.edu>
2024-10-10 09:43:10 -0700

    formatting size at age data

--- code/SizeAtAge.R
+++ code/SizeAtAge.R
@@ -7,11 +7,19 @@ size <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/environmental/SaA
 str(size)
 data <- read.csv(file = "~/Documents/GitHub/CSIA_lab_work/data/environmental/Ohlberger.csv")
 
-# Delete last row
+# Format size df
 size <- size[!size$Year %in% c(2023, 2024),]
+size <- size %>% select(Year, Pink_Salmon_lag1, Total_Run, X2.ocean_mass)
+size <- size %>% select(-X, -X.1, -X3.ocean_mass, -Summer_SST_lag1)
 
-# Delete last two columns
-size <- size[, -c(7, 8)]
+# Format data df
+data <- data %>% select(year, total_return, pink_tot_num, chum_tot_num)
+
+
+
+
+
+colnames(data) <- c("Year", "Wood", "Kvichak", "Egegik")
 
 # Update Ohlberger data with Woodard data
 
