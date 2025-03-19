ed3b4ff270467a13b5f4658a4a84df7b10686e75
Grace Henry <henryn98@uw.edu>
2025-03-19 16:23:57 -0700

    PDO plot

--- code/PDO_plot.R
+++ code/PDO_plot.R
@@ -8,10 +8,21 @@ setwd("~/Documents/GitHub/CSIA_lab_work/data/environmental")
 data <- read.csv("PDO_tidy.csv")
 
 # Plot data
-ggplot(data, aes(x = Year, y = Value)) +
-  geom_line() +
+ggplot(data, aes(x = Year, y = Value, fill = Value > 0)) +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black") +
+  geom_vline(xintercept = 1977, linetype = "dashed", color = "black") +
+  geom_col() +
+  scale_x_continuous(breaks = c(1965, 1975, 1985, 1995, 2005, 2015, 2022)) +
+  scale_fill_manual(values = c("TRUE" = "red2", "FALSE" = "darkblue")) +
   labs(title = "Pacific Decadal Oscillation (PDO) Index",
        x = "Year",
        y = "PDO Index") +
   theme_minimal() +
-  theme(plot.title = element_text(hjust = 0.5)) 
+  theme(
+    legend.position = "none",
+    axis.line.y = element_blank(),
+    plot.title = element_text(hjust = 0.5, family = "Times New Roman"), 
+    text = element_text(family = "Times New Roman") 
+  ) + 
+  annotate("text", x = 1980.5, y = -1.79, label = bquote(bold("1977 Shift")), family = "Times New Roman")
+
